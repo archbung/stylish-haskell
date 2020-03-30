8fb148399322752544e0bad925574d4abec9624a
Hizbullah Abdul Aziz <archbung@gmail.com>
2020-03-30 15:54:08 +0200

    Fix the case when modeline already exists

--- lib/Language/Haskell/Stylish/Step/VimModeline.hs
+++ lib/Language/Haskell/Stylish/Step/VimModeline.hs
@@ -3,11 +3,18 @@ module Language.Haskell.Stylish.Step.VimModeline
     ) where
 
 import Language.Haskell.Stylish.Step
+import Data.List
 
 
 step :: Int -> Step
-step spaces = makeStep "VimModeline" $ \ls _ -> modeline:ls
+step spaces = makeStep "VimModeline" $ \ls _ -> 
+    if existsModeline ls
+       then modeline:(tail ls)
+       else modeline:ls
     where
         modeline = "-- vim:set ts=" ++ show spaces ++
                    " sts=" ++ show spaces ++ " sw=" ++ show spaces ++ " et:"
 
+existsModeline :: Lines -> Bool
+existsModeline [] = False
+existsModeline (x:_) = "-- vim:" `isPrefixOf` x
--- tests/Language/Haskell/Stylish/Step/VimModeline/Tests.hs
+++ tests/Language/Haskell/Stylish/Step/VimModeline/Tests.hs
@@ -19,6 +19,7 @@ import           Language.Haskell.Stylish.Tests.Util
 tests :: Test
 tests = testGroup "Language.Haskell.Stylish.Step.VimModeline.Tests"
     [ testCase "case 01" case01
+    , testCase "case 02" case02
     ]
 
 
@@ -36,6 +37,28 @@ case01 = expected @=? testStep (step 4) input
 
     expected = unlines
         [ "-- vim:set ts=4 sts=4 sw=4 et:"
+        , "module Main"
+        , "    where"
+        , "data Foo"
+        , "    = Bar"
+        , "    | Qux"
+        ]
+
+-- | If there's a modeline already, overwrite it
+case02 :: Assertion
+case02 = expected @=? testStep (step 2) input
+  where
+    input = unlines
+        [ "-- vim:set ts=4 sts=4 sw=4 et:"
+        , "module Main"
+        , "    where"
+        , "data Foo"
+        , "    = Bar"
+        , "    | Qux"
+        ]
+
+    expected = unlines
+        [ "-- vim:set ts=2 sts=2 sw=2 et:"
         , "module Main"
         , "    where"
         , "data Foo"
